111e09a215d61923f3191ecd5f5dc9c5a2bb414a
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 10:10:32 +0300

    Add Net.Chdk.Meta.Writers.Json

--- src/Net.Chdk.Meta.Writers.Software.Json/JsonSoftwareWriter.cs
+++ src/Net.Chdk.Meta.Writers.Software.Json/JsonSoftwareWriter.cs
@@ -1,87 +1,29 @@
-﻿using Net.Chdk.Model.Software;
+﻿using Net.Chdk.Meta.Writers.Json;
+using Net.Chdk.Model.Software;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
-using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Net.Chdk.Meta.Writers.Software.Json
 {
-    sealed class JsonSoftwareWriter : ISoftwareWriter
+    sealed class JsonSoftwareWriter : JsonMetaWriter, ISoftwareWriter
     {
-        #region Static Constructor
-
-        static JsonSoftwareWriter()
-        {
-            _serializer = new Lazy<JsonSerializer>(GetSerializer);
-            _settings = new Lazy<JsonSerializerSettings>(GetSettings);
-        }
-
-        #endregion
-
         #region ISoftwareWriter Members
 
         public void WriteSoftware(string path, IDictionary<string, SoftwareInfo> hash2sw)
         {
-            Write(path, hash2sw);
+            WriteJson(path, hash2sw);
         }
 
         #endregion
 
-        #region Write
+        #region JsonMetaWriter Overrides
 
-        private static void Write<T>(string path, T obj)
+        protected override PreserveReferencesHandling PreserveReferencesHandling => PreserveReferencesHandling.All;
+
+        protected override IEnumerable<JsonConverter> GetConverters()
         {
-            using (var writer = new StreamWriter(path))
-            {
-                Serializer.Serialize(writer, obj);
-            }
-        }
-
-        #endregion
-
-        #region Serializer
-
-        private static readonly Lazy<JsonSerializer> _serializer;
-
-        private static JsonSerializer Serializer => _serializer.Value;
-
-        private static JsonSerializer GetSerializer()
-        {
-            return JsonSerializer.CreateDefault(Settings);
-        }
-
-        #endregion
-
-        #region Settings
-
-        private static readonly Lazy<JsonSerializerSettings> _settings;
-
-        public static JsonSerializerSettings Settings => _settings.Value;
-
-        private static JsonSerializerSettings GetSettings()
-        {
-            return new JsonSerializerSettings
-            {
-                Formatting = Formatting.Indented,
-                NullValueHandling = NullValueHandling.Ignore,
-                PreserveReferencesHandling = PreserveReferencesHandling.All,
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                Converters = GetConverters(),
-            };
-        }
-
-        #endregion
-
-        #region Converters
-
-        private static JsonConverter[] GetConverters()
-        {
-            return new[]
-            {
-                new VersionConverter()
-            };
+            yield return new VersionConverter();
         }
 
         #endregion
